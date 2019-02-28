c9b6ab40a60f3419dd93d3052a19b9692e6f6fcb
Frank Martinez <frankie.martinez@gmail.com>
2019-02-28 08:57:09 -0800

    scaffolding complete

---
+++ apple-nav/src/App.js
@@ -0,0 +1,24 @@
+import React, { Component, Fragment } from 'react';
+import './css/index.css';
+import NavWrapper from './components/NavWrapper';
+
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {}
+  }
+  render() {
+    return (
+      <Fragment>
+
+
+        <NavWrapper />
+
+
+
+      </Fragment>
+    );
+  }
+}
+
+export default App;
